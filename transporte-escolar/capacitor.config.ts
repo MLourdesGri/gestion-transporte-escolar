dab3ddf23b0787beddc4e849922bf674c010a562
MLourdesGri <grillilourdes@gmail.com>
2025-02-26 20:02:09 -0300

    borro tabs

--- transporte-escolar/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'transportes-escolares',
-  webDir: 'dist'
-};
-
-export default config;
