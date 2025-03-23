534f02313ff4351394b056ab0be43831d53c3005
rocifani <rociobfani@hotmail.com>
2025-03-23 19:15:43 -0300

    fixes styles confirmemail

--- gestion-transporte/src/views/ConfirmEmailPage.vue
+++ gestion-transporte/src/views/ConfirmEmailPage.vue
@@ -2,17 +2,19 @@
   <ion-page>
     <ion-content class="ion-padding">
       <div class="confirmation-box">
-        <h2 class="title single-line">Confirmación de Email</h2>
+        
    
         <div v-if="success" class="message success-message fixed-center">
+          <ion-icon :icon="checkmarkCircle" class="success-icon"></ion-icon>
           <p>¡Tu email ha sido confirmado correctamente!</p>
         </div>
         <div v-if="error" class="message error-message fixed-center">
+          <ion-icon :icon="closeCircle" class="error-icon"></ion-icon>
           <p>{{ error }}</p>
         </div>
 
         <div class="link-buttons fixed-bottom ">
-            <CustomButton @click="redirectToLogin">Ingresa</CustomButton>
+          <CustomButton @click="redirectToLogin">Ingresar</CustomButton>
         </div>
       </div>
     </ion-content>
@@ -20,83 +22,98 @@
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonContent, IonSpinner } from "@ionic/vue";
+import { IonPage, IonContent, IonIcon } from "@ionic/vue";
 import { ref, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import axios from "axios";
 import CustomButton from "@/components/CustomButton.vue";
-  
-  const route = useRoute();
-  const router = useRouter();
-  const loading = ref(true);
-  const success = ref(false);
-  const error = ref("");
-  
-  const confirmEmail = async () => {
-    const token = route.params.token;
-    try {
-      const API_URL = import.meta.env.VITE_API_URL;
-      const response = await axios.get(`${API_URL}/users/confirm-email/${token}`);
-      if (response.status === 200) {
-        success.value = true;
-      }
-    } catch (err: any) {
-      error.value = err.response?.data?.error.message || "Ocurrió un error inesperado";
-    } finally {
-      loading.value = false;
+import { checkmarkCircle, closeCircle } from "ionicons/icons";
+
+const route = useRoute();
+const router = useRouter();
+const success = ref(false);
+const error = ref("");
+
+const confirmEmail = async () => {
+  const token = route.params.token;
+  try {
+    const API_URL = import.meta.env.VITE_API_URL;
+    const response = await axios.get(`${API_URL}/users/confirm-email/${token}`);
+    if (response.status === 200) {
+      success.value = true;
     }
-  };
-  
-  const redirectToLogin = () => {
-    router.push("/login");
-  };
-  
-  onMounted(() => {
-    confirmEmail();
-  });
-  </script>
-  
-  <style scoped>
-  .message {
-    text-align: center;
-    font-size: 16px;
-    margin-top: 20px;
+  } catch (err: any) {
+    error.value = err.response?.data?.error.message || "Ocurrió un error inesperado";
   }
+};
+
+const redirectToLogin = () => {
+  router.push("/login");
+};
+
+onMounted(() => {
+  confirmEmail();
+});
+</script>
+
+<style scoped>
+.confirmation-box {
+  text-align: center;
+  padding: 20px;
+}
+
+.title {
+  font-size: 24px;
+  white-space: normal;
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+  width: 100%;
+}
+
+.message {
+  text-align: center;
+  font-size: 16px;
+  margin-top: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.success-message {
+  color: green;
+  font-weight: bold;
+  font-size: 30px;
+}
+
+.error-message {
+  color: red;
+  font-weight: bold;
+  font-size: 30px;
+}
+
+.success-icon {
+  color: green;
+  font-size: 100px;
+}
+
+.error-icon {
+  color: red;
+  font-size: 100px;
+}
+
+.link-buttons {
+  margin-left: 10px;
+  margin-right: 10px;
+}
   
-  .success-message {
-    color: green;
-    font-weight: bold;
-  }
+.fixed-bottom {
+  position: absolute;
+  bottom: 20px;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+
+
   
-  .error-message {
-    color: red;
-    font-weight: bold;
-  }
-  
-  .footer {
-    margin-top: 30px;
-    text-align: center;
-  }
-  
-  ion-card {
-    border-radius: 10px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  }
-  
-  ion-card-header {
-    border-radius: 10px 10px 0 0;
-    background-color: #003366;
-    color: white;
-  }
-  
-  ion-title {
-    font-family: 'Montserrat', sans-serif;
-    font-weight: 600;
-  }
-  
-  ion-button {
-    border-radius: 5px;
-    font-weight: bold;
-  }
-  </style>
-  
+</style>
