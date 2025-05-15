02057339cb0d64e7e3948c5ade846bc3eba501ce
Rocio Fani <rociobfani@hotmail.com>
2025-05-14 21:23:00 -0300

    '

--- gestion-transporte/src/services/api.ts
+++ gestion-transporte/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useUserStore } from "@/store/user";
 
-const API_URL = "http://gestion-transporte-escolar-be-production.up.railway.app"; 
+const API_URL = "http://localhost:3000"; 
 
 const api = axios.create({
   baseURL: API_URL,
